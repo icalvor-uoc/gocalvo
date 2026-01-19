dc62b2bf7d23116c5ab2d9971fcbaece47203e19
icalvor-uoc <62566684+icalvor-uoc@users.noreply.github.com>
2026-01-19 17:01:26 +0000

    .

---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/icalvor-uoc/gocalvo/submodule/saycat"
+
+func main() {
+  saycat.Saycat()
+}
---
+++ submodule/saycat.go
@@ -0,0 +1,7 @@
+package saycat
+
+import "fmt"
+
+func Saycat() {
+  fmt.Println("Cat")
+}
