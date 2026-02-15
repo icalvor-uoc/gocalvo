064ed744bdc0f40673470dc390acc06d7d0222ac
icalvor-uoc <62566684+icalvor-uoc@users.noreply.github.com>
2026-02-16 00:37:42 +0100

    .

---
+++ cmd/list_sum/main.go
@@ -0,0 +1,45 @@
+package main
+
+import "fmt"
+import "strconv"
+import "strings"
+// import "slices"
+import "os"
+import "bufio"
+import "package1"
+
+func main() {
+  package1.SayPackage()
+  fmt.Println(os.Args)
+  
+  reader := bufio.NewReader(os.Stdin)
+  var strsacc string = ""
+  var continueReading bool = true
+  for (continueReading) {
+      linebytes, isPrefix, lineError := reader.ReadLine()
+      
+      fmt.Println(len(linebytes))
+  
+      if (lineError != nil) {
+        panic(lineError)
+      }
+ 
+      var s string = string(linebytes)
+      strsacc = strsacc + s
+      continueReading = isPrefix
+  }
+  
+  strs := strings.Split(strsacc, " ")
+
+  var suma float32 = 0
+  for _, n := range strs {
+    f, e := strconv.ParseFloat(n, 32)
+    if (e != nil) {
+      fmt.Println(e)
+    } else {
+      suma += float32(f)
+    }
+  }
+  
+  fmt.Println("Resultado final:", suma)
+}
